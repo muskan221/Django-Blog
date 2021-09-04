5b64bea9064071a2c5ae2043a8a087846ea00efe
muskan221 <muskankhasturia15@gmail.com>
2021-09-05 02:21:55 +0530

    added page createcategory

--- posts/forms.py
+++ posts/forms.py
@@ -6,6 +6,8 @@ class PostForm(forms.ModelForm):
     class Meta:
         model =  Post
         fields = ['title', 'content', 'category'] 
+
+class CategoryForm(forms.ModelForm):
     
     class Meta:
         model = Category
